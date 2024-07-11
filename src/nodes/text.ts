4022d59b17e5a0dd8f7aef56fa3cbeef7be53c55
Louis Li <lee178lee@gmail.com>
2024-07-10 22:18:20 -0700

    Refactor transpileText function to handle URL replacement and line splitting

--- src/nodes/text.ts
+++ src/nodes/text.ts
@@ -1,5 +1,5 @@
-import type { DocumentNode } from "../types/default";
-import { replacePlaceholders, sanitizeText } from "../utils";
+import type { DocumentNode } from '../types/default';
+import { replacePlaceholders, sanitizeText } from '../utils';
 
 export default function transpileText(
   node: DocumentNode,
@@ -9,11 +9,29 @@ export default function transpileText(
   indexArray: number[]
 ): string {
   let result = node.text!;
-  if (config.sanitizetext) {
+  if (config.autoreplaceurl) {
+    if (isUrl(result)) {
+      result = replacePlaceholders(config.marks.a, { text: result });
+    }
+  } else if (config.sanitizetext) {
     result = sanitizeText(result);
   }
+
+  if (config.splitconnectedlines.character > 0) {
+    const splittext = result.split(' ');
+    result = '';
+    for (let i = 0; i < splittext.length; i++) {
+      let element = splittext[i];
+      if (element.length >= config.splitconnectedlines.character) {
+        result += replacePlaceholders(config.splitconnectedlines.command, { text: element }) + ' ';
+      } else {
+        result += element + (i < splittext.length - 1 ? ' ' : '');
+      }
+    }
+  }
+
   if (node.marks) {
-    node.marks.forEach((mark) => {
+    node.marks.forEach(mark => {
       const textRule = rules.marks[mark.type];
       if (textRule) {
         result = replacePlaceholders(textRule, { text: result });
@@ -21,4 +39,17 @@ export default function transpileText(
     });
   }
   return result;
+}
+//chatgpt generate stuff idk how this work
+function isUrl(text: string): boolean {
+  const urlPattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
+  return !!urlPattern.test(text);
 }
